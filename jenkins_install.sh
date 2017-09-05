d01183cf03ae36932c2d36771c9b03e5d59ac04e
山川 八大 <hachidaiyamakawa@gmail.com>
2017-09-05 21:59:35 +0900

    修正

--- jenkins_install.sh
+++ jenkins_install.sh
@@ -3,43 +3,25 @@
 # ssh config
 readonly SSH_CONFIG=~/.ssh/config
 cat <<EOT >${SSH_CONFIG}
-Host web
-  HostName 192.168.43.41
-  StrictHostKeyChecking no
-Host app
-  HostName 192.168.43.42
-  StrictHostKeyChecking no
-Host db1
-  HostName 192.168.43.43
-  StrictHostKeyChecking no
-Host db2
-  HostName 192.168.43.44
-  StrictHostKeyChecking no
-Host nosql1
-  HostName 192.168.43.45
-  StrictHostKeyChecking no
-Host nosql2
-  HostName 192.168.43.46
-  StrictHostKeyChecking no
 Host its
-  HostName 192.168.43.51
+  HostName 192.168.32.11
   StrictHostKeyChecking no
 Host vcs
-  HostName 192.168.43.52
+  HostName 192.168.32.12
   StrictHostKeyChecking no
 EOT
 
 chmod 600 ${SSH_CONFIG}
 
-# ssh key copy
+# ssh key create
 ssh-keygen -t rsa -N "" -f ~/.ssh/id_rsa
 
 # ansible install
 sudo yum install -y ansible
-ansible --version
-
-# git, jenkins install
-sudo yum install -y git java wget
+# git install
+sudo yum install -y git
+# jenkins install
+sudo yum install -y java wget
 sudo wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat/jenkins.repo
 sudo rpm --import https://jenkins-ci.org/redhat/jenkins-ci.org.key
 sudo yum install -y jenkins
